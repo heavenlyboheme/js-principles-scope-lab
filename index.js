a6e78edc90b64d499cb9df2ad7f7c2baeaa91bdd
github username <maria.kennedy@flatironschool.com>
2018-08-20 10:54:16 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "Greg";
+var bestCustomer;
+
+function upperCaseCustomerName() {
+  customerName = "BOB";
+};
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+
+
+};
+// overwriteBestCustomer(): See the consequences of declaring a variable in global scope, by writing a new function called overwriteBestCustomer() that changes that bestCustomer variable.
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+};
+
+//below is correct
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "Ryder";
+};
